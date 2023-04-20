162253c58d0cea384d69eab64ec4b9bcc1bc529e
SrBrianRs <SrBrianRs@outlook.com>
2023-04-20 16:55:43 -0500

    upload

---
+++ src/main/java/uv/org/programa02cc/Programa02CC.java
@@ -0,0 +1,17 @@
+package uv.org.programa02cc;
+
+/**
+ *
+ * @author SrBri
+ */
+public class Programa02CC {
+
+    public static void main(String[] args) {
+        
+        Singleton s = Singleton.getInstance();
+        
+        s.imprimir();
+        
+        //System.out.println("Hello Worldd!");
+    }
+}
